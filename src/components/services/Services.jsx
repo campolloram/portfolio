174458563688b6b8674ef41e8ba25240996d540d
root <xxx@mail.com>
2022-07-27 19:46:41 -0500

    Test

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,14 @@ import './services.css'
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I've worked on</h5>
+      <article className='service'>
+        <div className="service__head">
+          <h3>Search For Art</h3>
+        </div>
+      </article>
+    </section>
   )
 }
 
